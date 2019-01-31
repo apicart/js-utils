f0069c0146b49a68353b4b2f09cd05b33942ce9c
Vladimír Macháček <8machy@seznam.cz>
2019-01-31 23:42:48 +0100

    Fixed string validation for empty result.

--- src/components/validators/isEmpty.js
+++ src/components/validators/isEmpty.js
@@ -13,7 +13,7 @@ export function isEmpty(data) {
 		return false;
 
 	} else if (dataType === 'string') {
-		return !! data.length;
+		return data.length === 0;
 
 	} else if (dataType === 'object' || Array.isArray(data)) {
 		return Object.keys(data).length < 1;
