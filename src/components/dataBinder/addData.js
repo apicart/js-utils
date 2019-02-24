b0c2a944cfb80c316c101b02c3ed7195ce847dc5
Vladimír Macháček <8machy@seznam.cz>
2019-02-24 12:22:24 +0100

    Dom.on moved into the main file because of the wrong values scope generated by rollup

--- src/components/dataBinder/addData.js
+++ src/components/dataBinder/addData.js
@@ -1,17 +1,10 @@
 import {
-	DATA_BINDER_LOCAL_STORAGE_KEY,
-	DATA_BINDER_PREFIX,
-	DATA_BINDER_SELECTOR, dataBinder,
+	DATA_BINDER_LOCAL_STORAGE_KEY, dataBinder,
 	dataBinderData
 } from './dataBinder';
-import {dom} from '../dom/dom';
 import {objects} from '../objects/objects';
 import {json} from '../json/json';
 
-
-dom.on('change', DATA_BINDER_SELECTOR, function () {
-	addData(this.getAttribute(DATA_BINDER_PREFIX), this.value);
-});
 
 /**
  * @param {string} keyPath
