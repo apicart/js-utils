59cf28e7ae2dd90ca2bcfd6489ec2e4896a24f91
Vladimír Macháček <8machy@seznam.cz>
2019-02-24 12:22:44 +0100

    Added dom.on

--- src/components/dataBinder/dataBinder.js
+++ src/components/dataBinder/dataBinder.js
@@ -1,9 +1,10 @@
-// TODO solve multicheckbox saving
+// TODO solve multicheckbox and multiselect saving
 
 import {parse} from '../json/parse';
 import {addData} from './addData';
 import {bindData} from './bindData';
 import {removeData} from './removeData';
+import {dom} from "../dom/dom";
 
 export var DATA_BINDER_PREFIX = 'data-bind';
 export var DATA_BINDER_ELEMENTS_WITH_VALUE_ATTRIBUTE = ['select', 'input'];
@@ -13,6 +14,10 @@ export var DATA_BINDER_DATA_BOUND_ATTRIBUTE = 'data-bound';
 export var DATA_BINDER_BIND_IF_EMPTY_CONDITION = 'empty';
 export var dataBinderLocalStorageData = localStorage.getItem(DATA_BINDER_LOCAL_STORAGE_KEY);
 export var dataBinderData = dataBinderLocalStorageData ? parse(dataBinderLocalStorageData) : {};
+
+dom.on('change', DATA_BINDER_SELECTOR, function () {
+	addData(this.getAttribute(DATA_BINDER_PREFIX), this.value);
+});
 
 export var dataBinder = {
 	addData: addData,
